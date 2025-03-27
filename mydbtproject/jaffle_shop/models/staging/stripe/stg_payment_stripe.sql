fe2c4b187df904aea60d45a048d9cfd971191b92
gidee725 <cheruiyotgideon@outlook.com>
2025-03-27 15:39:39 +0300

    added fct_orders and dims_customers

--- mydbtproject/jaffle_shop/models/staging/stripe/stg_payment_stripe.sql
+++ mydbtproject/jaffle_shop/models/staging/stripe/stg_payment_stripe.sql
@@ -1,8 +1,11 @@
-select 
-id as payment_id,
-orderid as order_id,
-paymentmethod as payment_method,
-amount,
-created as payment_date,
-from {{ source('stripe', 'payment')}}
-where status = 'success'
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
+from raw.stripe.payment 
